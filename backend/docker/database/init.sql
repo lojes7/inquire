668a637e0e8ca76b59721a1ea18a7d9b5d58208c
lojes <18914412705@163.com>
2026-03-06 18:00:21 +0800

    修改了表结构，更改部分业务逻辑，此版本存在无法运行的严重bugs

--- backend/docker/database/init.sql
+++ backend/docker/database/init.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 7If4iLKASUsmIrqkae3mSelVe4UixeM0YZtACJnrEYee9oCadfdeK5axuEfHHmQ
+\restrict NKIwmDCaEUpCA19ybLwnpDVkVrKgayPfs7OjokcKnA4i3yBKH6yrU7dncY98L2e
 
 -- Dumped from database version 18.1 (Homebrew)
 -- Dumped by pg_dump version 18.1 (Homebrew)
@@ -38,10 +38,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: conversation_friends; Type: TABLE; Schema: public; Owner: -
---
-
---
 -- Name: conversation_users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -49,7 +45,6 @@ CREATE TABLE public.conversation_users (
     id bigint NOT NULL,
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
-    deleted_at timestamp without time zone,
     user_id bigint,
     conversation_id bigint,
     unread_count bigint DEFAULT 0,
@@ -67,7 +62,6 @@ CREATE TABLE public.conversations (
     id bigint NOT NULL,
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
-    deleted_at timestamp without time zone,
     type smallint
 );
 
@@ -80,7 +74,6 @@ CREATE TABLE public.files (
     id bigint NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     file_name character varying(255) NOT NULL,
     file_type character varying(50) NOT NULL,
     file_url character varying(255) NOT NULL,
@@ -118,13 +111,12 @@ CREATE TABLE public.friendship_requests (
     id bigint NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     sender_id bigint NOT NULL,
     receiver_id bigint NOT NULL,
     verification_message character varying(128),
     status character varying(16) NOT NULL,
     sender_name character varying(64) NOT NULL,
-    CONSTRAINT chk_friendship_requests_status CHECK (((status)::text = ANY ((ARRAY['pending'::character varying, 'accepted'::character varying, 'rejected'::character varying, 'canceled'::character varying])::text[])))
+    CONSTRAINT chk_friendship_requests_status CHECK (((status)::text = ANY (ARRAY[('pending'::character varying)::text, ('accepted'::character varying)::text, ('rejected'::character varying)::text, ('canceled'::character varying)::text])))
 );
 
 
@@ -155,7 +147,6 @@ CREATE TABLE public.friendships (
     id bigint NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     user_id bigint NOT NULL,
     friend_id bigint NOT NULL,
     friend_remark character varying(64) NOT NULL
@@ -189,7 +180,6 @@ CREATE TABLE public.message_users (
     id bigint NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     user_id bigint,
     message_id bigint,
     is_starred boolean DEFAULT false
@@ -225,8 +215,7 @@ CREATE TABLE public.messages (
     status smallint DEFAULT 0,
     id bigint NOT NULL,
     created_at timestamp with time zone,
-    updated_at timestamp with time zone,
-    deleted_at timestamp with time zone
+    updated_at timestamp with time zone
 );
 
 
@@ -254,7 +243,10 @@ ALTER SEQUENCE public.messages_id_seq OWNED BY public.messages.id;
 --
 
 CREATE TABLE public.texts (
-    text character varying(1024) CONSTRAINT texts_content_not_null NOT NULL,
+    id bigint NOT NULL,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    text character varying(1024) NOT NULL,
     message_id bigint NOT NULL
 );
 
@@ -272,10 +264,10 @@ CREATE TABLE public.users (
     phone_number character varying(20) NOT NULL,
     signature character varying(128),
     gender character varying(12),
-    deleted_at timestamp with time zone,
     created_at timestamp with time zone NOT NULL,
-    CONSTRAINT chk_users_gender CHECK (((gender)::text = ANY ((ARRAY['male'::character varying, 'female'::character varying, ''::character varying])::text[])))
+    CONSTRAINT chk_users_gender CHECK (((gender)::text = ANY (ARRAY[('male'::character varying)::text, ('female'::character varying)::text, (''::character varying)::text])))
 );
+
 
 --
 -- Name: conversation_users conversation_users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
@@ -334,18 +326,19 @@ ALTER TABLE ONLY public.messages
 
 
 --
+-- Name: texts texts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.texts
+    ADD CONSTRAINT texts_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: conversation_friends_user_id_friend_id_idx; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX conversation_friends_user_id_friend_id_idx ON public.conversation_friends USING btree (user_id, friend_id);
 
 
 --
@@ -370,45 +363,10 @@ CREATE INDEX idx_conversation_type ON public.conversations USING btree (type);
 
 
 --
--- Name: idx_conversation_users_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_conversation_users_deleted_at ON public.conversation_users USING btree (deleted_at);
-
-
---
 -- Name: idx_file_msg; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE UNIQUE INDEX idx_file_msg ON public.files USING btree (message_id);
-
-
---
--- Name: idx_files_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_files_deleted_at ON public.files USING btree (deleted_at);
-
-
---
--- Name: idx_friendship_requests_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_friendship_requests_deleted_at ON public.friendship_requests USING btree (deleted_at);
-
-
---
--- Name: idx_friendships_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_friendships_deleted_at ON public.friendships USING btree (deleted_at);
-
-
---
--- Name: idx_friendships_u_f; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX idx_friendships_u_f ON public.friendships USING btree (user_id, friend_id, deleted_at);
 
 
 --
@@ -419,13 +377,6 @@ CREATE UNIQUE INDEX idx_message_user ON public.message_users USING btree (user_i
 
 
 --
--- Name: idx_message_users_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_message_users_deleted_at ON public.message_users USING btree (deleted_at);
-
-
---
 -- Name: idx_messages_conversation_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -433,38 +384,10 @@ CREATE INDEX idx_messages_conversation_id ON public.messages USING btree (conver
 
 
 --
--- Name: idx_messages_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_messages_deleted_at ON public.messages USING btree (deleted_at);
-
-
---
 -- Name: idx_messages_sender_id; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE INDEX idx_messages_sender_id ON public.messages USING btree (sender_id);
-
-
---
--- Name: idx_requests_sender_receiver; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX idx_requests_sender_receiver ON public.friendship_requests USING btree (sender_id, receiver_id, deleted_at);
-
-
---
--- Name: idx_text_msg; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX idx_text_msg ON public.texts USING btree (message_id);
-
-
---
--- Name: idx_users_deleted_at; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX idx_users_deleted_at ON public.users USING btree (deleted_at);
 
 
 --
@@ -492,4 +415,5 @@ CREATE UNIQUE INDEX idx_users_uid ON public.users USING btree (uid);
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 7If4iLKASUsmIrqkae3mSelVe4UixeM0YZtACJnrEYee9oCadfdeK5axuEfHHmQ
+\unrestrict NKIwmDCaEUpCA19ybLwnpDVkVrKgayPfs7OjokcKnA4i3yBKH6yrU7dncY98L2e
+
